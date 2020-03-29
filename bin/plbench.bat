f990b4d5d18aed69334b9991f67d24409388e31a
Jose Zalacain <Jose Zalacain@DESKTOP-NK7F6C3>
2020-03-29 13:58:03 -0400

    Rename package to io.github.prolobjectlink

--- bin/plbench.bat
+++ bin/plbench.bat
@@ -5,4 +5,4 @@ for %%B in (%CURRENT_DIRECTORY%.) do set PROLOBJECTLINK_HOME=%%~dpB
 SET CLASSPATH=%PROLOBJECTLINK_HOME%lib\*
 
 : default jdk
-java -classpath %CLASSPATH% org.prolobjectlink.prolog.jpl.Swi7BenchmarkRunner %*
+java -classpath %CLASSPATH% io.github.prolobjectlink.prolog.jpl.Swi7BenchmarkRunner %*
